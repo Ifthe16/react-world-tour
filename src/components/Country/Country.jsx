b817bd5e2dcc3e0dbc700a54c5edddffc6f5e012
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-26 12:40:59 +0600

    added conditional style in country file of jsx and added state in countries file

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     // console.log(country);
     const { name, flags, population, area, cca3 } = country;
 
@@ -9,14 +9,16 @@ const Country = ({ country }) => {
     const handleVisited = () => {
         setVisited(!visited);
     }
-
+    console.log(handleVisitedCountry);
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'purple' : 'black' }}>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area}</p>
             <p>Code: {cca3}</p>
+            <button>Mark visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
